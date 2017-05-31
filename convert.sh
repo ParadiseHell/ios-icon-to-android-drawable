2ed5ca3a422a385298fc1fa40a4258c0aa49298a
ChengTao <chengtaolearn@163.com>
2017-05-31 23:25:11 +0800

    refactor:新增配置图标文件名特殊字符,用来区分具体drawable目录

--- convert.sh
+++ convert.sh
@@ -7,10 +7,7 @@ source ./config
 drawable_hdpi="drawable-hdpi"
 drawable_xhdpi="drawable-xhdpi"
 drawable_xxhdpi="drawable-xxhdpi"
-
-#regular expression
-regular_two="@2x."
-regular_three="@3x."
+drawable_xxxhdpi="drawable-xxxhdpi"
 
 #check if the file is try to convert,default 0 (no)
 can_file_convert=0
@@ -18,18 +15,34 @@ can_file_convert=0
 #the bumber of the image suffix
 image_suffix_num=0
 
+#define drawable category
+has_x=0
+has_xx=0
+has_xxx=0
+
 createAndroidDrawableFolder(){
 	mkdir -p $drawable_hdpi
-	mkdir -p $drawable_xhdpi
-	mkdir -p $drawable_xxhdpi
+	if [[ $has_x == 1 ]]
+	then
+		mkdir -p $drawable_xhdpi
+	fi
+	if [[ $has_xx == 1 ]]
+	then
+		mkdir -p $drawable_xxhdpi
+	fi
+	if [[ $has_xxx == 1 ]]
+	then
+		mkdir -p $drawable_xxxhdpi
+	fi
 }
 
 moveFile(){
 	#get the source image path
 	source_image_name=${1##*/}
 	#get the dest image name
-	dest_image_name=${source_image_name//@2x/}
-	dest_image_name=${dest_image_name//@3x/}
+	dest_image_name=${source_image_name//$xhdpi/}
+	dest_image_name=${dest_image_name//$xxhdpi/}
+	dest_image_name=${dest_image_name//$xxxhdpi/}
 	expect_image_name=""
 	for (( i=0; i<${#dest_image_name}; i++ ))
 	do
@@ -54,11 +67,18 @@ moveFile(){
 		mv -f -- "$1" "$drawable_xhdpi"
 		#rename file
 		mv -f -- "$drawable_xhdpi/$source_image_name" "$drawable_xhdpi/$expect_image_name"
-	else
+	elif [ $2 == 3 ]
+	then
 		#move image to speccail android drawable folder
 		mv -f -- "$1" "$drawable_xxhdpi"
 		#rename file
 		mv -f -- "$drawable_xxhdpi/$source_image_name" "$drawable_xxhdpi/$expect_image_name"
+
+	else
+		#move image to speccail android drawable folder
+		mv -f -- "$1" "$drawable_xxxhdpi"
+		#rename file
+		mv -f -- "$drawable_xxxhdpi/$source_image_name" "$drawable_xxxhdpi/$expect_image_name"
 	fi
 }
 
@@ -91,12 +111,15 @@ startConvert(){
 				checkFile "$file"
 				if [ $can_file_convert == 1 ]
 				then
-					if [[ "$file" =~ "$regular_two" ]]
+					if [[ $has_x == 1 && "$file" =~ "$xhdpi" ]]
 					then
 						moveFile "$file" 2
-					elif [[ "$file" =~ "$regular_three" ]]
+					elif [[ $has_xx == 1 && "$file" =~ "$xxhdpi" ]]
 					then
 						moveFile "$file" 3
+					elif [[ $has_xxx == 1 && "$file" =~ "$xxxhdpi" ]]
+					then
+						moveFile "$file" 4
 					else
 						moveFile "$file" 1
 					fi	
@@ -115,7 +138,23 @@ initConfig(){
 	drawable_hdpi=$android_drawable_directory/$drawable_hdpi
 	drawable_xhdpi=$android_drawable_directory/$drawable_xhdpi
 	drawable_xxhdpi=$android_drawable_directory/$drawable_xxhdpi
+	drawable_xxxhdpi=$android_drawable_directory/$drawable_xxxhdpi
 	image_suffix_num=${#image_suffix[@]}
+	#xhdpi
+	if [[ ! "$xhdpi" == "" ]]
+	then
+		has_x=1
+	fi
+	#xxhdpi
+	if [[ ! "$xxhdpi" == "" ]]
+	then
+		has_xx=1
+	fi
+	#xxxhdpi
+	if [[ ! "$xxxhdpi" == "" ]]
+	then
+		has_xxx=1
+	fi
 }
 
 main(){
